a6f00764aab312f2d641d3412532e430f4102ba8
SAMSUNGPROJECT\dgfl <dgfl@cin.ufpe.br>
2023-09-15 21:39:25 -0300

    instanciando o Flight

--- Controlador Assentos de Voo/src/Flight.java
+++ Controlador Assentos de Voo/src/Flight.java
@@ -1,8 +1,17 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Flight {
     private List<Seat> seats = new ArrayList<>();
+    public Flight(int numSeats) {
+        // Inicializa os assentos do voo com identificação
+        for (int i = 1; i <= numSeats; i++) {
+            seats.add(new Seat("Seat" + i));
+        }
+        // Embaralha a lista de assentos para que a alocação seja aleatória
+       // Collections.shuffle(seats);
+    }
 
 
 
--- Controlador Assentos de Voo/src/Main.java
+++ Controlador Assentos de Voo/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int numSeats =  10 ; // Número de assentos
+        int numberOfPassengers; // Quantidade de passageiros a serem embarcados
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Enter the number of passengers: ");
+            numberOfPassengers = scanner.nextInt();
+            if (numberOfPassengers < 0) {
+                System.out.println("Number of passengers cannot be negative.");
+            }
+        } while (numberOfPassengers < 0);
+
+        Flight flight = new Flight(numSeats);
+
     }
 }
